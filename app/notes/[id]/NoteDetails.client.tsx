a03a5531eb83b2b95acd1fe4ae4912b24c75048a
hlieb-kotiun <gleb.kotiun@gmail.com>
2026-05-09 21:43:41 +0200

    finish

---
+++ app/notes/[id]/NoteDetails.client.tsx
@@ -0,0 +1,42 @@
+"use client";
+import css from "./NoteDetails.module.css";
+import { fetchNoteById } from "@/lib/api";
+import { keepPreviousData, useQuery } from "@tanstack/react-query";
+import { useParams } from "next/navigation";
+
+const NoteDetailsClient = () => {
+  console.log(useParams());
+  const { id } = useParams();
+
+  const {
+    data: note,
+    isLoading,
+    isError,
+  } = useQuery({
+    queryKey: ["notes", id],
+    queryFn: () => {
+      return fetchNoteById(id as string);
+    },
+    placeholderData: keepPreviousData,
+  });
+
+  console.log(note);
+
+  return (
+    <div className={css.container}>
+      {isLoading && <p>Loading...</p>}
+      {isError && <p>Something went wrong.</p>}
+      {note && (
+        <div className={css.item}>
+          <div className={css.header}>
+            <h2>Note title</h2>
+          </div>
+          <p className={css.tag}>{note.tag}</p>
+          <p className={css.content}>{note.content}</p>
+          <p className={css.date}>{note.content}</p>
+        </div>
+      )}
+    </div>
+  );
+};
+export default NoteDetailsClient;
---
+++ components/TanStackProvider/TanStackProvider.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+interface TanStackProviderProps {
+  children: React.ReactNode;
+}
+
+const TanStackProvider = ({ children }: TanStackProviderProps) => {
+  const queryClient = new QueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+};
+export default TanStackProvider;
